875337791b51818d4913153499e12a7429aa5431
Fernando <gonzalezalvaro096@gmail.com>
2021-07-07 17:05:31 -0400

    settings

--- loginsystem/settings.py
+++ loginsystem/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 # DEBUG = env.bool('DEBUG', default=False)
-DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['ferloginsystem.herokuapp.com', 'localhost', '127.0.0.1']
 
